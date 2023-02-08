3449b43782e93ad6e32d4ca5c5d9c217f2fcb1e6
Alfred Arinze <fredintek@gmail.com>
2023-02-08 23:29:57 +0300

    made featuretop component flexible for other components

--- src/components/featuretop/FeatureTop.jsx
+++ src/components/featuretop/FeatureTop.jsx
@@ -1,15 +1,26 @@
 import "./FeatureTop.css";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
-const FeatureTop = ({ text, type }) => {
+const FeatureTop = ({ text, type, page }) => {
+  const navigate = useNavigate();
+
+  const handleViewAll = () => {
+    navigate("/view-all");
+  };
+
   return (
     <div className="feature__top">
       <h2 className={`${type}`}>{text}</h2>
 
-      {type === "market-place" ? (
+      {page === "view-all" || type === "market-place" || type === "featured" ? (
         <></>
       ) : (
-        <motion.div whileTap={{ scale: 0.8 }} className="feature__top-right">
+        <motion.div
+          onClick={handleViewAll}
+          whileTap={{ scale: 0.8 }}
+          className="feature__top-right"
+        >
           view all
         </motion.div>
       )}
