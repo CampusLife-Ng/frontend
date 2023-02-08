520deed0feba6a26ec4fd820e6d09bb8ac4cf045
Alfred Arinze <fredintek@gmail.com>
2023-02-08 23:21:20 +0300

    persisted liked lodge properties

--- src/App.js
+++ src/App.js
@@ -2,32 +2,50 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import {
-  selectIdLodgeList,
+  selectFeaturedLodgeList,
   selectLodgeList,
-  setIdOfLodges,
+  selectNormalLodgeList,
   setLodges,
 } from "./features/slices/lodgeSlice";
-import { Home } from "./pages";
+import { Home, Details, ViewAll, LikedPage, SignUp } from "./pages";
 
 function App() {
   const dispatch = useDispatch();
   const listOfLodge = useSelector(selectLodgeList);
+  const listOfFeatured = useSelector(selectFeaturedLodgeList);
+  const listOfNormal = useSelector(selectNormalLodgeList);
 
   // when application reloads persit liked-lodge-state
   const data = JSON.parse(window.localStorage.getItem("lodgeList")) || [];
+  const featuredLodgedata =
+    JSON.parse(window.localStorage.getItem("featuredLodgeList")) || [];
+  const normalLodgedata =
+    JSON.parse(window.localStorage.getItem("normalLodgeList")) || [];
   useEffect(() => {
-    dispatch(setLodges(data));
+    dispatch(setLodges({ data, featuredLodgedata, normalLodgedata }));
   }, []);
 
   // when lodgeList in redux state changes save directly to localstorage
   useEffect(() => {
     window.localStorage.setItem("lodgeList", JSON.stringify(listOfLodge));
-  }, [listOfLodge]);
+    window.localStorage.setItem(
+      "featuredLodgeList",
+      JSON.stringify(listOfFeatured)
+    );
+    window.localStorage.setItem(
+      "normalLodgeList",
+      JSON.stringify(listOfNormal)
+    );
+  }, [listOfLodge, listOfFeatured, listOfNormal]);
 
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/view-all" element={<ViewAll />} />
+        <Route path="/liked-lodges" element={<LikedPage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );
