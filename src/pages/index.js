f9785fc1aa4643e89e5950be543c0479e2994a11
Alfred Arinze <fredintek@gmail.com>
2023-02-08 23:39:34 +0300

    added details view-all liked and signup pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,5 @@
 export { default as Home } from "./home/Home";
+export { default as Details } from "./details/Details";
+export { default as ViewAll } from "./viewAll/ViewAll";
+export { default as LikedPage } from "./likedPage/LikedPage";
+export { default as SignUp } from "./signUp/SignUp";
